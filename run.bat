5430fe989e6323e5701fa7b200cce273812dd663
CCCCCasper <144883818+CCCCCasper@users.noreply.github.com>
2025-09-30 13:58:00 +0200

    nav bar inladen

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
 @echo off
 cd /d "%~dp0"
-start "" "C:\Users\caspe\OneDrive - ROC Drenthe College\Documents\Code\New-Project\Template\index.html"
+start "" "C:\Users\caspe\OneDrive - ROC Drenthe College\Documents\Code\New-Project\Template\index.php"
